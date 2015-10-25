d13927ac664ec7826f5c38e917b9b58915851eec
Zack <thurston@cwolf.cs.sonoma.edu>
2015-10-25 11:57:27 -0700

    jfdadf

--- script.js
+++ script.js
@@ -7,12 +7,46 @@ function ready(error, students) {
     console.log(students);
 }
 
-// // Create the Scale used for X Axis
-// var xScale = d3.scale.linear()
-//   .domain([0, 8])
-//   .rangeRoundBands([0, width], innerPadding, outerPadding);
-//
-// // Create the Scale used for Y Axis
-// var yScale = d3.scale.ordinal()
-//   .domain((Computer Science, Mathematics))
-//   .range([height, 0]);
+// Dimensional variable settings
+var margin = {
+  "top": 25,
+  "right": 20,
+  "bottom": 30,
+  "left": 70
+};
+
+// Create the Scale used for X Axis
+var xScale = d3.scale.linear()
+  .domain([0, 8])
+  .rangeRound([0, 1100]);
+
+// Create the Scale used for Y Axis
+var yScale = d3.scale.ordinal()
+  .domain(["Computer Science", "Mathematics"])
+  .rangeRoundBands([350, 0], 0.3, 0.25);
+
+  // Create Axes
+  var xAxis = d3.svg.axis()
+    .scale(xScale)
+    .orient("bottom");
+  var yAxis = d3.svg.axis()
+    .scale(yScale)
+    .orient("left");
+
+    // Create SVG Container for Graph
+var svgMain = d3.select("body").append("svg")
+  .attr("class", "svg")
+  .attr("width", 1100 + margin.left + margin.right)
+  .attr("height", 350 + margin.top + margin.bottom)
+  .append("g")
+  .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+
+// Append X and Y axes to SVG
+svgMain.append("g")
+  .attr("class", "axis")
+  .attr("transform", "translate(0," + 350 + ")")
+  .call(xAxis);
+svgMain.append("g")
+  .attr("id", "yAxis")
+  .attr("class", "axis")
+  .call(yAxis);
